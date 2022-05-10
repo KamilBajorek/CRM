8bdfe0d80a387a3f31b9a27285bfb64be763d637
KamilBajorek <elbajorro@gmail.com>
2022-05-10 18:32:22 +0200

    Initial commit

---
+++ src/test/java/com/bajorek_kalandyk/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajorek_kalandyk.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
