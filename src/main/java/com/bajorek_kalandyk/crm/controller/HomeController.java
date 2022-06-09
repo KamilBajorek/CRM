0e960800002c70f67b520d732c97f6dd86f3afc9
KamilBajorek <elbajorro@gmail.com>
2022-06-09 18:55:57 +0200

    Add request mapping to home page

--- src/main/java/com/bajorek_kalandyk/crm/controller/HomeController.java
+++ src/main/java/com/bajorek_kalandyk/crm/controller/HomeController.java
@@ -3,16 +3,12 @@ package com.bajorek_kalandyk.crm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController
 {
-    @GetMapping("")
-    public String homePage(Model model)
-    {
-        return "home";
-    }
-
+    @RequestMapping("/")
     @GetMapping("/")
     public String alsoHomePage(Model model)
     {
